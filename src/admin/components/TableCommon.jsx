64ea14e05daab75f966cd7e43466f07e32c31d87
Nitish Kumar <mehta21nk@gmail.com>
2024-02-26 17:04:05 +0530

    Initial Commit

--- src/admin/components/TableCommon.jsx
+++ src/admin/components/TableCommon.jsx
@@ -6,35 +6,7 @@ const TableCommon = (props) => {
 
   return (
     <table className='table_common'>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Image</th>
-          <th>Name</th>
-          <th>Action</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>01</td>
-          <td><img className='img-one' src="../images/img/about-1.jpg" alt="" /></td>
-          <td>Admin</td>
-          <td>
-            <SwitchBtn />
-          </td>
-          <td><span className="Active">Active</span></td>
-        </tr>
-        <tr>
-          <td>02</td>
-          <td><img className='img-one' src="../images/img/about-1.jpg" alt="" /></td>
-          <td>Admin</td>
-          <td>
-            <SwitchBtn />
-          </td>
-          <td><span className="DeActive">Deactive</span></td>
-        </tr>
-      </tbody>
+     {props.tblData}
     </table>
   )
 }
