55e033495154f5b60d914196f58f2011c07b80c3
Nitish Kumar <mehta21nk@gmail.com>
2024-02-26 18:01:03 +0530

    Banner Data

--- src/admin/components/AddData.jsx
+++ src/admin/components/AddData.jsx
@@ -5,7 +5,7 @@ const AddData = (props) => {
     return (
         <div className='add_box'>
             <div className='Label-box' ><span className='Lavel'>{props.Label} :</span></div>
-            <div className='Input-box'><input type={props.inputType} placeholder={props.Placeholder} /></div>
+            <div className='Input-box'><input onChange={props.changeFunction} type={props.inputType} placeholder={props.Placeholder} /></div>
         </div>
     )
 }
--- src/admin/pages/AddBanner.jsx
+++ src/admin/pages/AddBanner.jsx
@@ -8,11 +8,40 @@ import AddDataEditor from '../components/AddDataEditor';
 import Tinymce from '../components/Tinymce';
 
 const AddBanner = () => {
-    const [description, setDescription] = useState('');
+    const [name, setName] = useState('');
+	const [textField1, setTextField1] = useState('');
+	const [textField2, setTextField2] = useState('');
+	// const [file, setFile] = useState('');
+	const [description, setDescription] = useState('');
 
-    const setContent = (content) => {
-        setDescription(content);
-    }
+	const setContent = (content) => {
+		setDescription(content);
+	}
+    
+	const submitHandler = async (event) => {
+		event.preventDefault();
+
+		const data = new FormData();
+		data.append("name", name);
+		data.append("textField1", textField1);
+		data.append("textField2", textField2);
+		// data.append("file", file);
+		data.append("description", description);
+
+		const url = "http://localhost:5000/createBanner";
+
+		let result = await fetch(
+			url,
+			{
+				method: "POST",
+				body: data,
+			}
+		);
+
+		result = await result.json();
+
+		console.log(result);
+	}
     return (
         <section className='admin_container'>
             <Nav />
@@ -23,19 +52,28 @@ const AddBanner = () => {
                 <div className='content_box'>
                     <div className='content_container'>
                         <BreadCrumb pageName="Add Products" link="/admin" btnName="Manage Banners" />
-                        <div className='add_data'>
+                        <form onSubmit={submitHandler} className='add_data'>
                             <div className='row'>
                                 <div className='col-12'>
-                                    <AddData Width='w-50' Label="Banner Name" inputType="text" Placeholder="Name" />
+                                    <AddData Width='w-50' changeFunction={setName} Label="Banner Name" inputType="text" Placeholder="Name" />
+                                </div>
+                                <div className='col-12'>
+                                    <AddData Width='w-50' changeFunction={setTextField1} Label="Text Fields One" inputType="text" Placeholder="Text" />
+                                </div>
+                                <div className='col-12'>
+                                    <AddData Width='w-50' changeFunction={setTextField2} Label="Text Fields Two" inputType="text" Placeholder="Text" />
                                 </div>
                                 <div className='col-12'>
                                     <AddData Label="Banner Image" inputType="file" />
                                 </div>
                                 <div className='col-12'>
                                     <AddDataEditor Label="Banner Description" Editor={<Tinymce value="" description={setContent} />} />
+                                    <div className='row justify-content-end'>
+                                        <button  className='btn btn-primary mt-2 float-right w-fit col-auto mx-2'>Save Data</button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
